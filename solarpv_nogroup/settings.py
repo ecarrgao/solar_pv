990001f1b121bc34633abe2a8a18863525e3d9c7
Eduardo Carr-Gaona <ecarrgao@asu.edu>
2020-03-17 15:46:03 -0700

    Setup Django to use mysql database.

--- solarpv_nogroup/settings.py
+++ solarpv_nogroup/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'solarpv_nogroup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'solarpv_nogroup',
+        'USER': 'root',
+        'PASSWORD': 'Tony*11908',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
