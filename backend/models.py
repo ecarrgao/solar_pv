9a0c9903428b55cbd237912551d0728739b073ba
ecarrgao <ecarrgao@asu.edu>
2020-04-06 22:30:23 -0700

    Fixed bugs __str__ function.

--- backend/models.py
+++ backend/models.py
@@ -2,31 +2,31 @@ from django.db import models
 from solarpv.models import *
 
 class Location(models.Model):
-    address1 = models.CharField(max_length=20)
-    address2 = models.CharField(max_length=20)
-    city = models.CharField(max_length=20)
+    address1 = models.CharField(max_length=30)
+    address2 = models.CharField(max_length=30)
+    city = models.CharField(max_length=50)
     state = models.CharField(max_length=2)
     postal_code = models.IntegerField()
     phone_number = models.CharField(max_length=15)
     fax_number = models.CharField(max_length=15)
-    client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.address1
+        return str(self.client)
 
 class TestStandard(models.Model):
-    standard_name = models.CharField(max_length=20)
-    description = models.CharField(max_length=20)
+    standard_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
     published_date = models.DateField()
 
     def __str__(self):
-        return self.standard_name
+        return str(self.standard_name)
 
 class Product(models.Model):
-    model_number = models.CharField(primary_key=True, max_length=7)
-    product_name = models.CharField(max_length=20)
-    cell_technology = models.CharField(max_length=20)
-    cell_manufacturer = models.CharField(max_length=20)
+    model_number = models.CharField(max_length=7)
+    product_name = models.CharField(max_length=50)
+    cell_technology = models.CharField(max_length=50)
+    cell_manufacturer = models.CharField(max_length=50)
     number_of_cells = models.IntegerField()
     number_of_cells_in_series = models.IntegerField()
     number_of_series_strings = models.IntegerField()
@@ -34,29 +34,29 @@ class Product(models.Model):
     product_length = models.FloatField()
     product_width = models.FloatField()
     product_weight = models.FloatField()
-    superstrate_type = models.CharField(max_length=20)
-    superstrate_manufacturer = models.CharField(max_length=20)
-    substrate_type = models.CharField(max_length=20)
-    substrate_manufacturer = models.CharField(max_length=20)
-    frame_type = models.CharField(max_length=20)
-    frame_adhesive = models.CharField(max_length=20)
-    encapsulant_type = models.CharField(max_length=20)
-    encapsulant_manufacturer = models.CharField(max_length=20)
-    junction_box_type = models.CharField(max_length=20)
-    junction_box_manufacturer = models.CharField(max_length=20)
+    superstrate_type = models.CharField(max_length=50)
+    superstrate_manufacturer = models.CharField(max_length=50)
+    substrate_type = models.CharField(max_length=50)
+    substrate_manufacturer = models.CharField(max_length=50)
+    frame_type = models.CharField(max_length=50)
+    frame_adhesive = models.CharField(max_length=50)
+    encapsulant_type = models.CharField(max_length=50)
+    encapsulant_manufacturer = models.CharField(max_length=50)
+    junction_box_type = models.CharField(max_length=50)
+    junction_box_manufacturer = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.product_name
+    def __str__ (self):
+        return str(self.model_number)
 
 class TestSequence(models.Model):
-    sequence_name = models.CharField(max_length=20)
+    sequence_name = models.CharField(max_length=25)
 
     def __str__(self):
-        return self.sequence_name
+        return str(self.sequence_name)
 
 class PerformanceData(models.Model):
     model_number = models.ForeignKey(Product, on_delete=models.CASCADE)
-    sequence_id = models.ForeignKey(TestSequence, on_delete=models.CASCADE)
+    sequence = models.ForeignKey(TestSequence, on_delete=models.CASCADE)
     max_system_voltage = models.FloatField()
     voc = models.FloatField()
     isc = models.FloatField()
@@ -66,25 +66,25 @@ class PerformanceData(models.Model):
     ff = models.FloatField()
 
     def __str__(self):
-        return self.model_number
+        return str(self.model_number)
 
 class Service(models.Model):
-    service_name = models.CharField(max_length=20)
-    description = models.CharField(max_length=20)
+    service_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
     is_fl_required = models.BooleanField()
     fl_frequency = models.FloatField()
-    standard_id = models.ForeignKey(TestStandard, on_delete=models.CASCADE)
+    standard = models.ForeignKey(TestStandard, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.service_name
+        return str(self.service_name)
 
 class Certificate(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     report_number = models.IntegerField()
     issue_date = models.DateField()
-    standard_id = models.ForeignKey(TestStandard, on_delete=models.CASCADE)
-    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    standard = models.ForeignKey(TestStandard, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     model_number = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.report_number
+        return str(self.user)
--- solarpv/models.py
+++ solarpv/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.validators import MaxValueValidator
-from backend.models import *
 
 PREFIX = [
     {"Mr.", "Mr"},
