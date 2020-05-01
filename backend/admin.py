1f29302ab384ed49985c37ae7c86b82e26a1bbe7
Eduardo Carr-Gaona <ecarrgao@asu.edu>
2020-05-01 06:52:10 -0700

    Removed unused files.

--- backend/admin.py
+++ backend/admin.py
@@ -1,4 +1,2 @@
 from django.contrib import admin
-from backend.models import TestStandard
-
-admin.site.register(TestStandard)
+#
--- backend/apps.py
+++ backend/apps.py
@@ -1,5 +1,4 @@
-from django.apps import AppConfig
+from django.contrib import admin
+from backend.models import TestStandard
 
-
-class BackendConfig(AppConfig):
-    name = 'backend'
+admin.site.register(TestStandard)
